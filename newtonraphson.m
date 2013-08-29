d5861127b5cd9b0caefa0e2357451a75c3567102
Mark Mikofski <mark.mikofski@sunpowercorp.com>
2013-08-29 14:21:50 -0700

    v0.2 update

---
+++ newtonraphson.m
@@ -0,0 +1,123 @@
+function [x, resnorm, evalf, output, jacob] = newtonraphson(fun, x0, options)
+% NEWTONRAPHSON Solve set of non-linear equations using Newton-Raphson method.
+%
+% FUN is a function that returns a vector of residuals of the governing
+% equations and takes a vector, x, as its only argument. When the equations are
+% solved by x, then F(x) == zeros(size(x(:), 1)). If FUN is an anonymous
+% function it must not use DEAL, because there's no way to check NARGOUT.
+%
+% Optionally FUN may return the Jacobian, Jij = dFi/dxj, as an additional
+% output. If FUN only returns one output, then J is estimated using a center
+% difference approximation,
+%   Jij = dFi/dxj = (Fi(xj + dx) - Fi(xj - dx))/2/dx.
+% The Jacobian should be a square matrix whose columns correspond to d/dxj and
+% whose rows correspond to dFi/d.
+% EG:  J23 = dF2/dx3 is the 2nd row ad 3rd column.
+%
+% X0 is a vector of initial guesses.
+%
+% OPTIONS is a structure of solver options created using OPTIMSET.
+% EG: options = optimset('TolX', 0.001).
+%
+% The following options can be set:
+% * OPTIONS.TOLFUN is the maximum tolerance of the norm of the residuals
+%   [1e-6].
+% * OPTIONS.MAXITER is the maximum number of iterations before giving up
+%   [100]
+% * OPTIONS.DISPLAY sets the level of display: {'off', 'iter'}
+%   ['iter']
+%
+% X is the solution that solves the set of equations within the given tolerance.
+% RESNORM is norm(FEVAL) and FEVAL is F(X). OUTPUT is a structure
+% containing the number of iterations and JACOB is the J(X).
+%
+% See also OPTIMSET, OPTIMGET, FMINSEARCH, FZERO, FMINBND, FSOLVE, LSQNONLIN
+%
+% References:
+% http://en.wikipedia.org/wiki/Newton's_method
+% http://en.wikipedia.org/wiki/Newton's_method_in_optimization
+%
+% Version 0.2
+% * Remove `options.FinDiffRelStep` and `options.TypicalX` since not in MATLAB.
+% * Set `dx = eps^(1/3)` in `jacobian` function.
+% * Remove `options` argument from `funwrapper` & `jacobian` functions
+%   since no longer needed.
+% * Set typx = x0; typx(x0==0) = 1; % use initial guess as typx, if 0 use 1.
+% * Replace `feval` with `evalf` since `feval` is builtin.
+%% initialize
+% There are no argument checks!
+x0 = x0(:); % needs to be a column vector
+% set default options
+oldopts = optimset( ...
+    'TolFun', 1e-6, 'MaxIter', 100, 'Display', 'iter');
+if nargin<3
+    options = oldopts; % use defaults
+else
+    options = optimset(oldopts, options); % update default with user options
+end
+F = @(x)funwrapper(fun, x); % wrap FUN so it always returns J
+%% get options
+tol = optimget(options, 'TolFun'); % tolerance
+maxiter = optimget(options, 'MaxIter'); % max number of iterations
+display = strcmpi('iter', optimget(options, 'Display')); % display iterations
+typx = x0; % x scaling value is the initial guess
+typx(x0==0) = 1; % remove zeros from typical x
+%% set scaling values
+Ftyp = F(typx); % typical f
+J0 = F(typx)*(1./typx'); % Jacobian scaling matrix
+%% set display
+if display
+    fprintf('\n%10s %10s %10s %12s\n', 'Niter', 'resnorm', 'stepnorm', ...
+        'convergence')
+    for n = 1:45,fprintf('-'),end,fprintf('\n')
+    printout = @(n, r, s, c)fprintf('%10d %10.4g %10.4g %12.4g\n', n, r, s, c);
+end
+%% check initial guess
+x = x0; % initial guess
+[evalf, J] = F(x); % evaluate initial guess
+resnorm = norm(evalf); % calculate norm of the residuals
+%% solver
+Niter = 0; % start counter
+if display,printout(Niter, resnorm, 0, 0);end
+while resnorm>tol && Niter<maxiter
+    Niter = Niter+1; % increment counter
+    Jstar = J./J0; % scale Jacobian
+    Fstar = evalf./Ftyp; % scale F
+    dx_star = -Jstar\Fstar; % Newton-Raphson solver
+    dx = dx_star.*typx; % rescale x
+    x = x+dx; % next guess
+    [evalf, J] = F(x); % evaluate residuals
+    resnorm0 = resnorm; % old resnorm
+    resnorm = norm(evalf); % calculate new resnorm
+    convergence = log(resnorm0/resnorm); % calculate convergence rate
+    stepnorm = norm(dx); % norm of the step
+    if display,printout(Niter, resnorm, stepnorm, convergence);end
+end
+%% output
+output.iterations = Niter;
+jacob = J;
+end
+
+function [evalf, J] = funwrapper(fun, x)
+% if nargout<2 use finite differences to estimate J
+if abs(nargout(fun))>=2
+    [evalf, J] = fun(x);
+else
+    evalf = fun(x);
+    J = jacobian(fun, x); % evaluate center diff if no Jacobian
+end
+evalf = evalf(:); % needs to be a column vector
+end
+
+function J = jacobian(fun, x)
+% estimate J
+dx = eps^(1/3); % finite difference delta
+dof = size(x, 1); % degrees of freedom
+J = zeros(dof); % a square matrix
+for n = 1:dof
+    % create a vector of deltas, change delta_n by dx
+    delta = zeros(dof, 1); delta(n) = delta(n)+dx;
+    dF = fun(x+delta)-fun(x-delta); % delta F
+    J(:, n) = dF/dx/2; % derivatives dF/d_n
+end
+end
---
+++ newtonraphson_example.m
@@ -0,0 +1,53 @@
+%% newton raphson example
+% Find the Darcy friction factor for pipe flow using the Colebrook
+% equation.
+
+%% References:
+% * http://en.wikipedia.org/wiki/Darcy_friction_factor_formulae
+% * http://en.wikipedia.org/wiki/Darcy%E2%80%93Weisbach_equation
+% * http://en.wikipedia.org/wiki/Moody_chart
+% * http://www.mathworks.com/matlabcentral/fileexchange/35710-iapwsif97-functional-form-with-no-slip
+%% inputs:
+p = 0.68; % [MPa] water pressure (100 psi)
+dp = -0.068*1e6; % [Pa] pipe pressure drop (10 psi)
+T = 323; % [K] water temperature
+D = 0.10; % [m] pipe hydraulic diameter
+L = 100; % [m] pipe length
+roughness = 0.00015; % [m] cast iron pipe roughness
+rho = 1./IAPWS_IF97('v_pT',p,T); % [kg/m^3] water density (988.1 kg/m^3)
+mu = IAPWS_IF97('mu_pT',p,T); % [Pa*s] water viscosity (5.4790e-04 Pa*s)
+Re = @(u) rho*u*D/mu; % Reynolds number
+%% governing equations
+% Use Colebrook and Darcy-Weisbach equation to solve for pipe flow.
+
+% friction factor (Colebrook eqn.)
+residual_friction = @(u, f) 1/sqrt(f) + 2*log10(roughness/3.7/D + 2.51/Re(u)/sqrt(f));
+% pressure drop (Darcy-Weisbach eqn.)
+residual_pressdrop = @(u, f) rho*u^2*f*L/2/D + dp;
+% residuals
+fun = @(x) [residual_friction(x(1),x(2)), residual_pressdrop(x(1),x(2))];
+%% solve
+x0 = [2.5,0.02]; % initial guess
+[x, resnorm, feval, output, jacob] = newtonraphson(fun, x0);
+%% results
+fprintf('Outputs:\n')
+properties = {'Pressure','Pressure-drop','Temperature','Diameter','Length', ...
+    'roughness','density','viscosity','Reynolds-number','speed','friction'};
+units = {'[Pa]','[Pa]','[C]','[cm]','[m]','[mm]','[kg/m^3]','[Pa*s]','[]','[m/s]','[]'};
+values = {p*1e6,dp,T-273.15,D/100,L,roughness*1000,rho,mu,Re(x(1)),x(1),x(2)};
+fprintf('%15s %10s %10s\n','Property','Unit','Value')
+results = [properties; units; values];
+fprintf('%15s %10s %10.4g\n',results{:})
+%% comparison
+% solve using Haaland
+Ntest = 10;
+u0 = linspace(x(1)*0.1, x(1)*10, Ntest); % [m/s]
+Re0 = Re(u0);
+f0 = (1./(-1.8*log10((roughness/D/3.7)^1.11 + 6.9./Re0))).^2;
+u0 = sqrt(-dp/rho./(f0*L/2/D));
+% plot
+plot(u0, f0, '-', u0, x(2)*ones(1,Ntest), '--', x(1)*ones(1,Ntest), f0, '--')
+grid
+title('Pipe flow solution using Haaland equation.')
+xlabel('water speed, u [m/s]'),ylabel('friction factor, f')
+legend('f_{Haaland}','f = 0.1024', 'u = 1.159 [m/s]')
